71bc237e03f9d363f88e1a57b140347df6653993
KilliannH <killiannhervagault@gmail.com>
2023-12-07 22:16:35 +0100

    change active menu by active route

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -9,8 +12,18 @@ export class NavbarComponent implements OnInit {
 
   @Input()
   appName: string = '';
+  route: string = '';
 
-  constructor() { }
+  constructor(private router: Router, private location: Location) {
+    of(router.events.subscribe((val) => {
+      if(location.path() != ''){
+        this.route = location.path();
+      } else {
+        this.route = '/'
+      }
+      console.log(this.route);
+   }));
+  }
 
   ngOnInit(): void {
   }
